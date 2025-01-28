45c78544061b17c959af0f13117e1383b2ba1941
RecursionExcursion <rloup15@gmail.com>
2025-01-27 19:26:21 -0500

    shrunk width of process inputs

--- src/components/DeployableCreator.tsx
+++ src/components/DeployableCreator.tsx
@@ -7,6 +7,28 @@ import Button from "./Button";
 import Input from "./Input";
 import Select from "./Select";
 
+const spinner = (
+  <div role="status">
+    <svg
+      aria-hidden="true"
+      className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+      viewBox="0 0 100 101"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+        fill="currentColor"
+      />
+      <path
+        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+        fill="currentFill"
+      />
+    </svg>
+    <span className="sr-only">Loading...</span>
+  </div>
+);
+
 const createProcess = (): Process => {
   return {
     type: "path",
@@ -16,9 +38,12 @@ const createProcess = (): Process => {
 
 export default function DeployableCreator() {
   const [processes, setProcesses] = useState<Process[]>([]);
+  const [loaded, setLoaded] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     addProcess();
+    setLoaded(true);
   }, []);
 
   function handleInputChange(
@@ -76,43 +101,54 @@ export default function DeployableCreator() {
   const controlInterface = () => {
     return (
       <div className="flex gap-4">
-        <Button onClick={async () => await downloadExecutable(processes)}>
-          Create Executable
+        <Button
+          disabled={loading}
+          onClick={async () => {
+            setLoading(true);
+            await downloadExecutable(processes);
+            setLoading(false);
+          }}
+        >
+          {loading ? spinner : "Create Executable"}
         </Button>
         <Button onClick={addProcess}>Add Process</Button>
-        <Button onClick={() => console.log(processes)}>Extract</Button>
       </div>
     );
   };
 
   return (
-    <div className="w-[80%] flex flex-col gap-5 justify-center items-center">
-      {controlInterface()}
-      <div className="flex flex-col gap-5 w-full">
-        {processes.map((proc, i) => {
-          return (
-            <div key={proc.type + proc.type + i} className="flex gap-5 items-center align-middle justify-center">
-              <Select
-                value={proc.type}
-                onChange={(e: ChangeEvent<HTMLSelectElement>) =>
-                  handleSelectChange(e, proc)
-                }
+    loaded && (
+      <div className="w-[50%] flex flex-col gap-5 justify-center items-center">
+        {controlInterface()}
+        <div className="flex flex-col gap-5 w-full">
+          {processes.map((proc, i) => {
+            return (
+              <div
+                key={proc.type + proc.type + i}
+                className="flex gap-5 items-center align-middle justify-center"
               >
-                <option value={"path"}>Path</option>
-                <option value={"cmd"}>Command</option>
-              </Select>
-              <Input
-                value={proc.arg}
-                onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                  handleInputChange(e, proc)
-                }
-                type="text"
-              />
-              <Button onClick={(e) => removeProcess(e, proc)}>Remove</Button>
-            </div>
-          );
-        })}
+                <Select
+                  value={proc.type}
+                  onChange={(e: ChangeEvent<HTMLSelectElement>) =>
+                    handleSelectChange(e, proc)
+                  }
+                >
+                  <option value={"path"}>Path</option>
+                  <option value={"cmd"}>Command</option>
+                </Select>
+                <Input
+                  value={proc.arg}
+                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                    handleInputChange(e, proc)
+                  }
+                  type="text"
+                />
+                <Button onClick={(e) => removeProcess(e, proc)}>Remove</Button>
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </div>
+    )
   );
 }
