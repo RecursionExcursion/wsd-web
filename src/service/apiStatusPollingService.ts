01ed3e18c4896d6a3d0063db469f573858b14eeb
RecursionExcursion <rloup15@gmail.com>
2025-04-01 14:33:55 -0400

    made check so app doesnt dcontually poll

--- src/service/apiStatusPollingService.ts
+++ src/service/apiStatusPollingService.ts
@@ -2,15 +2,25 @@ import { emitter } from "../lib/events/EventEmittor";
 import { eventKeys } from "../lib/events/events";
 
 export const pollApiStatus = async () => {
+  const res = await fetch("/api/status");
+  if (res.ok) {
+    const isReady = await res.json();
+    if (isReady) {
+        console.log(`API is already warmed up`)
+      emitter.emit(eventKeys.backendReady, { content: isReady });
+      return;
+    }
+  }
+
   const pollFreq = 5000;
 
   const poll = setInterval(async () => {
-    console.log("Polling API");
+    console.log("polling...");
     const res = await fetch("/api/status");
 
     if (res.ok) {
-      console.log("API response received");
       const isReady = await res.json();
+      console.log("API is ready: ", isReady);
 
       if (isReady) {
         emitter.emit(eventKeys.backendReady, { content: isReady });
