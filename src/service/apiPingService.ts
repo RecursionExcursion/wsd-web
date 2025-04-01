a26a87c5659a11e6997c6e6082c4d003c5d37d48
RecursionExcursion <rloup15@gmail.com>
2025-04-01 14:20:16 -0400

    rm unused var

--- src/service/apiPingService.ts
+++ src/service/apiPingService.ts
@@ -1,20 +1,8 @@
 "use server";
 
 import { loadEnvVar } from "./externalAPIService";
-import { getApiRoute } from "./getRoutesService";
 
 // Hits base route to trigger cold start
 export async function wakeupApi() {
   await fetch(await loadEnvVar("API_BASE"));
 }
-/* Builds and caches cross compilation binaries in Go for faster
- * first build time after container spin up
- */
-// export async function warmUpApi() {
-//   await fetch(await getApiRoute("getWarmUp"));
-// }
-
-// export async function isWarmedUp() {
-//   const res = await fetch(await getApiRoute("getStatus"));
-//   return (await res.json()) as boolean;
-// }
