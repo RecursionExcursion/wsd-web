b9535d16d465880584355980dd2966e57e724cd6
RecursionExcursion <rloup15@gmail.com>
2025-04-01 21:24:08 -0400

    fix build step

--- src/service/dummyRequestService.ts
+++ src/service/dummyRequestService.ts
@@ -1,29 +1,29 @@
-import { Process } from "../types/process";
-import { DDWsdPayload, downloadExecutable } from "./downloadService";
+// import { Process } from "../types/process";
+// import { DDWsdPayload } from "./downloadService";
 
-export async function sendDummy() {
-  const ddPayload: DDWsdPayload = {
-    name: "dummy",
-    arch: "win64",
-    commands: ([{ type: "path", arg: "www.facebook.com" }] as Process[]).map(
-      (p) => {
-        const prefix = p.type === "cmd" ? "cmd:" : "url:";
+// export async function sendDummy() {
+//   const ddPayload: DDWsdPayload = {
+//     name: "dummy",
+//     arch: "win64",
+//     commands: ([{ type: "path", arg: "www.facebook.com" }] as Process[]).map(
+//       (p) => {
+//         const prefix = p.type === "cmd" ? "cmd:" : "url:";
 
-        let arg = p.arg;
-        if (p.type === "cmd") {
-          arg = arg.replaceAll("\\", "/");
-        }
+//         let arg = p.arg;
+//         if (p.type === "cmd") {
+//           arg = arg.replaceAll("\\", "/");
+//         }
 
-        return prefix + arg;
-      }
-    ),
-  };
+//         return prefix + arg;
+//       }
+//     ),
+//   };
 
-  const res = await fetch("/api/download", {
-    method: "POST",
-    body: JSON.stringify(ddPayload),
-    cache: "no-store",
-  });
+//   const res = await fetch("/api/download", {
+//     method: "POST",
+//     body: JSON.stringify(ddPayload),
+//     cache: "no-store",
+//   });
 
-  console.log("res", res);
-}
+//   console.log("res", res);
+// }
